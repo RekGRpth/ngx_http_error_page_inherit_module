849fbf270bd488eb2a7eaa45e144e56693325448
RekGRpth <rekgrpth@gmail.com>
2022-02-16 09:11:06 +0500

    -log

--- ngx_http_error_page_inherit_module.c
+++ ngx_http_error_page_inherit_module.c
@@ -11,10 +11,6 @@ static char *ngx_conf_set_flag_slot_my(ngx_conf_t *cf, ngx_command_t *cmd, void 
     ngx_http_error_page_inherit_loc_conf_t *lcf = conf;
     ngx_http_core_loc_conf_t *clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     lcf->error_pages = clcf->error_pages;
-    ngx_http_err_page_t *elts = clcf->error_pages->elts;
-    for (ngx_uint_t i = 0; i < clcf->error_pages->nelts; i++) {
-        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "args[%i] = %V", i, &elts[i].value.value);
-    }
     return ngx_conf_set_flag_slot(cf, cmd, conf);
 }
 
@@ -48,7 +44,6 @@ static char *ngx_http_error_page_inherit_merge_loc_conf(ngx_conf_t *cf, void *pa
         for (ngx_uint_t i = 0; i < prev->error_pages->nelts; i++) {
             if (!(err = ngx_array_push(clcf->error_pages))) return NGX_CONF_ERROR;
             *err = elts[i];
-            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "args[%i] = %V", i, &elts[i].value.value);
         }
     }
     conf->error_pages = clcf->error_pages;
